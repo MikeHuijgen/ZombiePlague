dda95cefe571b507bd25150b5a16207ba70ceb80
MikeHuijgen <mikeh162001@hotmail.com>
2024-12-29 16:18:34 +0100

    First Prototype push

---
+++ Assets/Scripts/Gun/Gun.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    [SerializeField] private int damage;
+
+    public void Shoot()
+    {
+        
+    }
+}
